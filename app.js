015ba4544d85830aacf233298de10076521fe52a
shingaryu <shingaryu@gmail.com>
2020-05-04 14:26:03 +0900

    Create a new node.js project for Express API server

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const pokemonStrategiesController = require('./controllers/pokemonStrategiesController');
+
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+app.use(function(req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+app.get('/', function(req, res) {
+  res.send('Welcome to matchup-chart-api!');
+});
+
+app.listen(8000, function() {
+  console.log('Example app listening on port 8000!');
+});
+
+app.use('/pokemonStrategies', pokemonStrategiesController);
